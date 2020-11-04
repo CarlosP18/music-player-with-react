294460f1cee9d248eab8f0c241e61aa9a1dcc35a
Carlos Plaza <carlosfplaza@hotmail.com>
2020-11-04 17:55:13 -0300

    se crea estrcutura visual de reproductor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './App.css';
+import { ButtonsBar } from './button-bar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ButtonsBar />
   );
 }
 
